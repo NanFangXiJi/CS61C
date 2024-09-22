886e08751f1ce4be7ed1792fd8986f6f97394990
NanFang <alfiellier@protonmail.com>
2024-09-22 22:35:42 +0800

    finished lab2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t a = *reg >> 2;
+    uint16_t b = a >> 1;
+    uint16_t c = b >> 2;
+    uint16_t d = (*reg ^ a ^ b ^ c) & 1;
+    uint16_t e = ~((d ^ 1) << 15);
+    uint16_t f = d << 15;
+
+    *reg = ((*reg >> 1) | f) & e;
 }
 
