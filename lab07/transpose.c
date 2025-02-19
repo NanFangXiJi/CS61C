e5aa589bc55d2b08036bca99b6458fa5abd4c38c
NanFang <alfiellier@protonmail.com>
2025-02-19 22:41:09 +0800

    finished lab07

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -13,4 +13,18 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    for (int br = 0; br * blocksize < n; br++) {
+        for (int bc = 0; bc * blocksize < n; bc++) {
+            for (int x = 0; x < blocksize; x++) {
+                for (int y = 0; y < blocksize; y++) {
+                    int p1 = x + br * blocksize;
+                    int p2 = y + bc * blocksize;
+                    if (p1 >= n || p2 >= n) {
+                        continue;
+                    }
+                    dst[p2 + p1 * n] = src[p1 + p2 * n];
+                }
+            }
+        }
+    }
 }
